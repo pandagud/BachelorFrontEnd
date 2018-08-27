a0091acfc8be7d5340fe0f396b37e4783869f48f
Jacob Lindvald Pedersen <au520517@uni.au.dk>
2018-08-27 14:15:29 +0200

    Added Default Test

---
+++ BachelorFrontEndTest/DefaultTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace BachelorFrontEndTest
+{
+    [TestFixture]
+    public class DefaultTest
+    {
+        [Test]
+        public void BasicDefaultTest()
+        {
+            Assert.AreEqual(4,4);
+        }
+    }
+}
---
+++ FrontEndTest/DefaultTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace FrontEndTest
+{
+    [TestFixture]
+    public class DefaultTest
+    {
+        [Test]
+        public void BasicDefaultTest()
+        {
+            Assert.AreEqual(4, 4);
+        }
+    }
+}
